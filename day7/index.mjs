7d232d9d770a5acbd8004fc740227b89d3e48143
Edoardo Gargano <edoardo.gargano@vimncontractor.com>
2021-12-17 14:57:32 +0100

    day 7

---
+++ day7/index.mjs
@@ -0,0 +1,19 @@
+import { readFile, getFile } from "../lib/file.mjs";
+
+const input = readFile(getFile("input.txt", import.meta.url))
+  .split(",")
+  .map(Number);
+
+let resultList = [];
+
+const getDifferences = (num) => {
+  return input.reduce((acc, curr) => {
+    return Math.abs((curr - num)) + acc
+  }, 0)
+}
+
+input.forEach((elm, pos) => {
+  resultList[pos] = getDifferences(elm)
+});
+
+console.log("day7", "solution:", Math.min(...resultList));
