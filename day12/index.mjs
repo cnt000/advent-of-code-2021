fe114452218ddd27f03502e283f14e5b2ee88e4a
Edoardo Gargano <cnt000@gmail.com>
2022-12-01 01:18:14 +0100

    day 12 - 2021

--- day12/index.mjs
+++ day12/index.mjs
@@ -6,13 +6,12 @@ const testInput = readFile(getFile('test-input.txt', import.meta.url)).trim()
 const input = readFile(getFile('input.txt', import.meta.url)).trim()
 
 const doTheThing = (input) => {
+  const results = []
   const uniqueValues = [
     ...new Set(input.replaceAll('\n', '-').split('-').filter(Boolean))
   ]
-  console.log('🚀 ~ file: Untitled-2:15 ~ uniqueValues', uniqueValues)
 
   const lines = input.split('\n')
-  console.log('🚀 ~ file: day12.js:15 ~ lines', lines)
 
   const getLinksFor = (value) =>
     lines
@@ -21,13 +20,66 @@ const doTheThing = (input) => {
         line.replace(new RegExp(`\\b${value}\\b`, 'g'), '').replaceAll('-', '')
       )
       .filter((value) => value !== 'start')
-  console.log("🚀 ~ file: day12.js:22 ~ getLinksFor('start')", getLinksFor('d'))
 
   const nodes = uniqueValues.map((value) => ({
     value,
     links: getLinksFor(value)
   }))
-  console.log('🚀 ~ file: day12.js:17 ~ nodes', nodes)
+  // console.log('🚀 ~ file: day12.js:17 ~ nodes', nodes)
+
+  const getNode = (value) => nodes.find((node) => node.value === value)
+
+  const visit = (node, path) => {
+    const { value, links } = node
+    if (/^[a-z]+$/.test(value) && path.includes(value)) {
+      return path
+    }
+    const updatedPath = [...path, value]
+    if (value === 'end') {
+      results.push(updatedPath)
+      // console.log('🚀 ~ file: index.mjs:44', results)
+      return path
+    }
+    links.forEach((link) => visit(getNode(link), updatedPath))
+  }
+
+  const hasLowerCaseCouple = (path) => {
+    const items = path.filter((item) => /[a-z]/g.test(item))
+    const unique = new Set(items)
+    return unique.size !== items.length
+  }
+
+  const visitSecondPart = (node, path) => {
+    const { value, links } = node
+
+    if (
+      /^[a-z]+$/.test(value) &&
+      hasLowerCaseCouple(path) &&
+      path.includes(value)
+    ) {
+      return path
+    }
+    const updatedPath = [...path, value]
+    if (value === 'end') {
+      results.push(updatedPath)
+      return path
+    }
+    links.forEach((link) => visitSecondPart(getNode(link), updatedPath))
+  }
+
+  visitSecondPart(getNode('start'), [])
+  console.log(
+    'Part 1:',
+    // results,
+    results.length
+  )
+
+  visitSecondPart(getNode('start'), [])
+  console.log(
+    'Part 2:',
+    // results,
+    results.length
+  )
 }
 
 doTheThing(input)
